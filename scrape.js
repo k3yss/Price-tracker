d107edc8534d37db6e30c659088020fb19a31512
k3ys <rsi.sxs@gmail.com>
2022-06-24 19:31:23 +0530

    remove .env file

--- scrape.js
+++ scrape.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
-console.log(accountSid, authToken);
+// console.log(accountSid, authToken);
 const url =
   "https://www.flipkart.com/apple-2020-macbook-pro-m1-8-gb-256-gb-ssd-mac-os-big-sur-myd82hn-a/p/itmef224bcc599dc?pid=COMFXEKMTGHAGSVX&lid=LSTCOMFXEKMTGHAGSVXHVO7KQ&marketplace=FLIPKART&q=macbook+pro&store=6bo%2Fb5g&srno=s_1_1&otracker=search&otracker1=search&fm=organic&iid=aac2da8f-c5d9-438f-bbbe-c50a3217d5e9.COMFXEKMTGHAGSVX.SEARCH&ppt=hp&ppn=homepage&ssid=yyzs6qdjg00000001656075428027&qH=9cf36a97583bd48f";
 
@@ -27,20 +27,21 @@ async function scrape() {
     .text()
     .replace(/[,.₹​]/g, "");
   //   console.log("Testing");
-  console.log(price);
+  //   console.log(price);
   const priceNum = parseInt(price);
   product.price = priceNum;
-  console.log(product);
+  //   console.log(product);
   //   Send an SMS
-  if (priceNum < 200000) {
+  const exPrice = 200000;
+  if (priceNum < exPrice) {
     client.messages
       .create({
-        body: `The price of ${product.name} went below ${price}. Purchase it at ${product.link}`,
+        body: `The price of ${product.name} went below ${exPrice}. Purchase it at ${product.link}`,
         from: "+14698334080",
         to: "+917480016255",
       })
       .then((message) => {
-        console.log(message.sid);
+        console.log(message);
         clearInterval(handle);
       });
   }
